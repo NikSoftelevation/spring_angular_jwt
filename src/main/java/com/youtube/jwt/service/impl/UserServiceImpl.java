5ee5dc184473118d00f7743c5800d2a0070f046c
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-06-15 18:07:17 +0530

    Update UserServiceImpl.java

--- src/main/java/com/youtube/jwt/service/impl/UserServiceImpl.java
+++ src/main/java/com/youtube/jwt/service/impl/UserServiceImpl.java
@@ -5,14 +5,20 @@ import com.youtube.jwt.repository.UserRepository;
 import com.youtube.jwt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public User createNewUser(User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
         return userRepository.save(user);
     }
 }
